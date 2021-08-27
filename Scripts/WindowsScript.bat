c6dd17a51dcad4cae4cbe8fac2a52524c96cd083
Yash Bhavsar <yash@Yashs-MBP.domain.name>
2021-08-27 15:59:23 +0530

    adding windows bat file

---
+++ Scripts/WindowsScript.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set /p pemFilePath=Enter .pem file path: 
+set /p zipFilePath=Enter Validator Key zip file path: 
+set /p instanceCredential=Enter AWS public IPv4 and external DNS hostname[eg: ec2-user@ec2-x-xx-xxx-xxx.compute-1.amazonaws.com]: 
+
+
+scp -i %pemFilePath% -r %zipFilePath% %instanceCredential%:/home/ec2-user
+
+
+
+ssh -i %pemFilePath% %instanceCredential%
