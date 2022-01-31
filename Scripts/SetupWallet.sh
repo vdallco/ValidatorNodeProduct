85cd09d9bb7af0b15da51e69f57d3daf32c25831
asharoo1 <84122970+asharoo1@users.noreply.github.com>
2022-01-31 16:50:07 +0530

    Changing the docker image version to v2.0.2

--- Scripts/SetupWallet.sh
+++ Scripts/SetupWallet.sh
@@ -24,9 +24,9 @@ function validate_password() {
                 
                 docker rm validatorr
                 
-                sudo docker run -it -v $HOME/Eth2Validators/prysm-wallet-v2:/wallet -v /home/ec2-user/password:/password --network="host" gcr.io/prysmaticlabs/prysm/validator:v1.4.3 wallet create --wallet-dir=/wallet --accept-terms-of-use --wallet-password-file=/password/pass.txt
+                sudo docker run -it -v $HOME/Eth2Validators/prysm-wallet-v2:/wallet -v /home/ec2-user/password:/password --network="host" gcr.io/prysmaticlabs/prysm/validator:v2.0.2 wallet create --wallet-dir=/wallet --accept-terms-of-use --wallet-password-file=/password/pass.txt
                 
-                sudo docker run -it -v $HOME/validator_keys:/keys -v $HOME/Eth2Validators/prysm-wallet-v2:/wallet -v $HOME/Eth2:/validatorDB -v /home/ec2-user/password:/password --name validatorr gcr.io/prysmaticlabs/prysm/validator:v1.4.3 --datadir=/validatorDB accounts import --keys-dir=/keys --wallet-dir=/wallet --accept-terms-of-use --wallet-password-file=/password/pass.txt
+                sudo docker run -it -v $HOME/validator_keys:/keys -v $HOME/Eth2Validators/prysm-wallet-v2:/wallet -v $HOME/Eth2:/validatorDB -v /home/ec2-user/password:/password --name validatorr gcr.io/prysmaticlabs/prysm/validator:v2.0.2 --datadir=/validatorDB accounts import --keys-dir=/keys --wallet-dir=/wallet --accept-terms-of-use --wallet-password-file=/password/pass.txt
                 sudo chown -R ec2-user:ec2-user /home/ec2-user/Eth2 /home/ec2-user/Eth2Validators
                 sudo chmod 700 /home/ec2-user/Eth2 /home/ec2-user/Eth2Validators
                 
